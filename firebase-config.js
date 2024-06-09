fdccf40d7068cdbf51df2680559f8e1da2f66632
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-06-09 18:07:05 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase konfigürasyonu
 var firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+apiKey: "AIzaSyAZBd9ppfpc7WKZWcNiSUtnR1pvD9bSOVM",
+  authDomain: "betatest-4f394.firebaseapp.com",
+  projectId: "betatest-4f394",
+  storageBucket: "betatest-4f394.appspot.com",
+  messagingSenderId: "551760858025",
+  appId: "1:551760858025:web:3547727feb020d32bf52b8"
 };
 
 // Firebase başlatma
