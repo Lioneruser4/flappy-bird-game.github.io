2aa6da10d9b93cc65d7ead4693b38c609b3d8798
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-06-10 18:44:52 +0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,17 +1,8 @@
-function register() {
-    var username = document.getElementById('register-username').value;
-    var password = document.getElementById('register-password').value;
-    // Kayıt işlemleri
-    console.log('Kayıt: Kullanıcı Adı: ' + username + ', Şifre: ' + password);
-    // Kayıt olduktan sonra giriş formunu göster
-    document.getElementById('login-form').style.display = 'block';
-}
-
 function login() {
-    var username = document.getElementById('login-username').value;
+    var email = document.getElementById('login-email').value;
     var password = document.getElementById('login-password').value;
     // Giriş işlemleri
-    console.log('Giriş: Kullanıcı Adı: ' + username + ', Şifre: ' + password);
+    console.log('Giriş: E-posta: ' + email + ', Şifre: ' + password);
     // Giriş yaptıktan sonra profil formunu göster
     document.getElementById('profile').style.display = 'block';
 }
