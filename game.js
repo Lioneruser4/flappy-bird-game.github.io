2c9363eeda0af2796848c2fc4931d550530e9082
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-06-21 23:40:38 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,54 +1,42 @@
-const canvas = document.getElementById('gameCanvas');
+// game.js
+const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const birdImg = new Image();
-birdImg.src = 'images/bird.png';
+let player = { x: 50, y: canvas.height - 150, width: 50, height: 50, speed: 5 };
+let coins = [];
+let obstacles = [];
+let score = 0;
+let gameSpeed = 3;
 
-const backgroundImg = new Image();
-backgroundImg.src = 'images/background.png';
-
-const bird = {
-    x: 50,
-    y: 50,
-    size: 20,
-    gravity: 0.5,
-    lift: -15,
-    velocity: 0,
-    show: function() {
-        ctx.drawImage(birdImg, this.x, this.y, this.size, this.size);
-    },
-    up: function() {
-        this.velocity += this.lift;
-    },
-    update: function() {
-        this.velocity += this.gravity;
-        this.velocity *= 0.9;
-        this.y += this.velocity;
-        
-        if (this.y > canvas.height - this.size) {
-            this.y = canvas.height - this.size;
-            this.velocity = 0;
-        }
-        
-        if (this.y < 0) {
-            this.y = 0;
-            this.velocity = 0;
-        }
-    }
-};
-
-window.addEventListener('click', () => {
-    bird.up();
-});
-
-function draw() {
-    ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
-    bird.show();
-    bird.update();
-    requestAnimationFrame(draw);
+function drawPlayer() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
 }
 
-draw();
+function drawCoins() {
+    ctx.fillStyle = 'gold';
+    coins.forEach(coin => {
+        ctx.beginPath();
+        ctx.arc(coin.x, coin.y, 10, 0, Math.PI * 2);
+        ctx.fill();
+    });
+}
+
+function drawObstacles() {
+    ctx.fillStyle = 'black';
+    obstacles.forEach(obstacle => {
+        ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+    });
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPlayer();
+    drawCoins();
+    drawObstacles();
+    requestAnimationFrame(update);
+}
+
+update();
