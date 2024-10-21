1d1fc3935c97c1159a455ddb9fb0ae24448fcc11
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-10-21 15:52:47 +0400

    Update game.js

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ let plane = {
 };
 
 let planeImg = new Image();
-planeImg.src = 'path_to_image.png';  // Burada təyyarənin şəklini qoymalısınız
+planeImg.src = 'https://www.google.com/search?q=plane+png&client=ms-android-xiaomi-terr1-rso2&sca_esv=0ba5b4315e0bd468&udm=2&biw=360&bih=729&ei=XUAWZ9m5F8jEwPAPjI_doQw&oq=plane+png&gs_lp=EhNtb2JpbGUtZ3dzLXdpei1zZXJwIglwbGFuZSBwbmcyBRAAGIAEMgUQABiABDIEEAAYHjIEEAAYHjIEEAAYHkjMDVBCWJUMcAF4AJABAZgB3gSgAZcTqgEHMi0zLjUtM7gBA8gBAPgBAZgCBKACuQaYAwCSBwcxLjAuMi4xoAe_Dw&sclient=mobile-gws-wiz-serp#vhid=VITCw9LjJ45iKM&vssid=mosaic';  // Burada təyyarənin şəklini qoymalısınız
 
 function drawPlane() {
     ctx.drawImage(planeImg, plane.x, plane.y, plane.width, plane.height);
