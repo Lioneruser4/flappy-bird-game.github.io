401ae16bc490dc2308537c11d223e8a95cf7af83
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2025-03-22 15:30:55 +0400

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ def download():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=False)  # Debug modunu kapatın
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
