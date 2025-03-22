f8316b3f3340fa1a4c4015cd7c8ac26156f43908
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2025-03-22 15:23:05 +0400

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def download():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)  # Debug modunu kapatın
