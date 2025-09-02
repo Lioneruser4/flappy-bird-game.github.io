6d42f96e62de14246f96ce85f17ce57287136174
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2025-09-02 22:19:12 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-   module.exports = {
-     ADMIN_ID: "976640409", // Admin ID'nizi buraya ekleyin
-     token: "5741055163:AAHjaluUJsYOKy7sDdMlVnGabFFMtBAF_UQ" // Bot token'ınızı buraya ekleyin
-   };
